fc74c4cb6b877fa6464f6823dc097236a75eb5bc
Michael Hoffman <michaelhoffman@Michaels-MacBook-Air.local>
2019-03-10 17:05:03 -0600

    Add complete task function

--- main.js
+++ main.js
@@ -2,19 +2,20 @@ let state = {
     "lists": [{
         "id": 1,
         "tasks": [{
+            "id": '1-1',
             "completed": false,
             "deteted": false,
             "description": "Description goes here for task"
             
 
         },
-        {
+        {   "id": '1-2',
             "completed": false,
             "deteted": false,
             "description": "Description TWO goes here for task"
             
         },
-        {
+        {   "id": '1-3',
             "completed": false,
             "deteted": false,
             "description": "Description THREE goes here for task"
@@ -28,6 +29,11 @@ let state = {
 ]
 }
 
+function completeTask(id){
+    let taskElement = $(`#${id}`)
+    let input = taskElement.find('input');
+    input.prop("disabled", true)
+}
 
 $( document ).ready(function() { console.log('ready');
    let lists = $('.lists'); console.log(lists);
@@ -37,7 +43,14 @@ $( document ).ready(function() { console.log('ready');
        let listElement = emptyList.clone()
        list.tasks.forEach(task => {
            let taskElement = emptyTask.clone()
-           taskElement.append(`<button type="button" class="btn btn-primary">Complete</button>`);
+           taskElement.attr("id", task.id); 
+           let completeButton = $(`<button type="button" class="btn btn-primary">Complete</button>`);
+           completeButton.click(event => {
+            completeTask(task.id)
+           })
+
+           taskElement.append(completeButton);
+           taskElement.append(`<button type="button" class="btn btn-secondary">Delete</button>`);
            taskElement.append(`<div clas="completeTask"><input></input></div>`);
            let input = taskElement.find('input');
            input.val(task.description); console.log(task); console.log(taskElement);
